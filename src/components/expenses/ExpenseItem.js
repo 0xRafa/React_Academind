bba9414c475ce1f613983e2e97bb364846e36b44
0xRafa <rafa@fluent.finance>
2023-01-19 08:41:50 -0300

    48. Working with "state"

--- src/components/expenses/ExpenseItem.js
+++ src/components/expenses/ExpenseItem.js
@@ -1,23 +1,23 @@
+import { useState } from "react";
 import { ExpenseDate } from "./ExpenseDate";
 import { Card } from "../UI/Card";
 import "./ExpenseItem.css";
 
 function ExpenseItem({ title, amount, date }) {
-	
+	const [itemTitle, setItemTitle] = useState(title);
+
 	const clickHandler = () => {
-		console.log("cliqued")
-	}
-	
+		itemTitle == title ? setItemTitle("updated") : setItemTitle(title);
+	};
+
 	return (
 		<Card className="expense-item">
 			<ExpenseDate date={date} />
 			<div className="expense-item__description">
-				<h2>{title}</h2>
+				<h2>{itemTitle}</h2>
 				<div className="expense-item__price">${amount}</div>
 			</div>
-			<button onClick={clickHandler}>
-				Change title
-			</button>
+			<button onClick={clickHandler}>Change title</button>
 		</Card>
 	);
 }
