32061ad20f3896655362803eeec6fb423029c58b
0xRafa <rafa@fluent.finance>
2023-01-19 17:57:11 -0300

    two way binding

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -26,7 +26,10 @@ export const ExpenseForm = () => {
 			amount: enteredAmount,
 			date: new Date(enteredDate),
 		};
-		console.log(expenseData)
+		
+		setEnteredTitle('')
+		setEnteredAmount('')
+		setEnteredDate('')
 	};
 
 	return (
@@ -34,7 +37,7 @@ export const ExpenseForm = () => {
 			<div className="new-expense__controls">
 				<div className="new-expense__control">
 					<label>Title</label>
-					<input type="text" onChange={titleChangeHandler} />
+					<input type="text" value={enteredTitle} onChange={titleChangeHandler} />
 				</div>
 			</div>
 			<div className="new-expense__controls">
@@ -44,6 +47,7 @@ export const ExpenseForm = () => {
 						type="number"
 						min="0.01"
 						step="0.01"
+						value={enteredAmount}
 						onChange={amountChangeHandler}
 					/>
 				</div>
@@ -55,6 +59,7 @@ export const ExpenseForm = () => {
 						type="date"
 						min="2019-01-01"
 						max="2022-12-31"
+						value={enteredDate}
 						onChange={dateChangeHandler}
 					/>
 				</div>
